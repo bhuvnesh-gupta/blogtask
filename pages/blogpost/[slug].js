4888012b195bf059011190e90e953e0bec0facd0
Bhuvnesh Gupta <120572645+bhuvnesh-gupta@users.noreply.github.com>
2023-04-07 00:28:54 +0530

    myblog

---
+++ pages/blogpost/[slug].js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react'
+import { useRouter } from 'next/router'
+import styles from '@/styles/BlogPost.module.css'
+
+const slug = () => {
+  const [blog, setBlog] = useState();
+  const router = useRouter();
+  useEffect(() => {
+    if (!router.isReady) return;
+      const { slug } = router.query;
+      fetch(`http://localhost:3000/api/getBlogs?slug=${slug}`).then((a) =>{
+        return a.json();})
+        .then((parsed) =>{
+          console.log(parsed)
+          setBlog(parsed)
+        })
+  },[router.isReady])
+  
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+      <h1>{blog && blog.title}</h1>
+      <hr />
+      <div>{blog && blog.content}</div>
+      </main>
+    </div>
+  )
+}
+export default slug;
